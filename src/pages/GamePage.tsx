2a2ac54e7e7b0aaf08c84e4cbd05dca34e1de9bc
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-26 15:39:05 +0300

    test#1

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,5 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000", { autoConnect: false });
 
 export default function GamePage({ name }: { name: string }) {
   const { gameId } = useParams();
@@ -11,14 +14,53 @@ export default function GamePage({ name }: { name: string }) {
     }
   }, []);
 
-  
+  const [connected, setConnected] = useState(false);
+
+  const [playerOneName, setPlayerOneName] = useState(null); // owner (have function to destroy room)
+  const [playerTwoName, setPlayerTwoName] = useState(null);
+
+  const [move, setMove] = useState<string>(); // "Player1" || "Player2"
+  const [winner, setWinner] = useState(null); // "Player1" || "Player2" || "Ничья"
+
+  useEffect(() => {
+    if (name) {
+      socket.connect();
+      socket.emit("join", { gameId, name });
+      //   setConnected(true);
+    }
+  }, []);
+
+  //   useEffect(() => {
+  //     socket.on("connection", (socket) => {});
+  //   }, [connected]);
+
+  socket.on("get game", (game) => {
+    console.log(game);
+  });
+
+  // Взять имена игроков с сервера! Потом:
+  useEffect(() => {
+    // if(!1 || name !== 1) == занять
+    // else if(!2 || name !== 1) == занять
+    // else redirect
+  }, []);
+
+  // connect to game
 
   return (
     <div>
-      // Если не залогинен то идем на логин пэйдж -- оттуда редирект назад на игру // Вытгиваем
-      query парам из url // Подключаемся к игре // TODO: нужно определить создателя комнаты и у
-      создателя есть кнопка удалить игру // В UI обаратываем 2 юзера -- больше нельзя // Если юзер
-      выходит - горит оффлайн флаг
+      {/* <button
+        onClick={() => {
+          socket.emit("get game", gameId);
+        }}
+      >
+        get game
+      </button> */}
+      {/* room link */}
+      {/* player 1 name; online marker; if move === p1 == marker */}
+      {/* game */}
+      {/* player 2 name; online marker; if move === p2 == marker */}
+      {/* if winner = botton new game == enable, else == disable */}
     </div>
   );
 }
