6f65af69e236f8db196dd04fa8dc0ba5b0002096
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-29 00:43:01 +0300

    update production link

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import CreateGamePage from "./pages/CreateGamePage";
 import GamePage from "./pages/GamePage";
 import LoginPage from "./pages/LoginPage";
 
-export const socket = io("https://task7-server.onrender.com", {
+export const socket = io("https://task7-server.dzmitryrev.repl.co", {
   autoConnect: false,
   transports: ["websocket"],
 });
